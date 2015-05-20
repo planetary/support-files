91b8440122fd60eb76a5feb74f7b1418810b2d89
Radu Dan <dan@planetary.io>
2015-05-20 18:29:20 +0300

    Add nodejs stage scripts

---
+++ stage/nodejs-pm2.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+project="kepler"
+
+# kill server to perform migrations
+pm2 show $project
+exists=$?
+if [ $exists -eq 0 ]; then
+    pm2 stop $project
+fi
+
+cd www
+git fetch
+git pull
+npm install
+gulp build
+migrate up
+cd ..
+
+# restart server if previously running, else register it for the first time
+if [ $exists -eq 0 ]; then
+    pm2 restart $project
+else
+    pm2 start $project.json
+fi
